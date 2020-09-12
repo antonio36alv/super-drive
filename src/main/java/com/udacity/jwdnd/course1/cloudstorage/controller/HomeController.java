967eb44dc6c03ffa02afb9c7d4cdbb55bbf5f250
Antonio Alvarado <ant.36alv@gmail.com>
2020-09-11 21:56:55 -0400

    working on db queries

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/HomeController.java
@@ -2,7 +2,10 @@ package com.udacity.jwdnd.course1.cloudstorage.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 @Controller
 @RequestMapping("/home")
@@ -11,6 +14,11 @@ public class HomeController {
     @GetMapping
     public String getHomePage() {
         return "home";
+    }
+
+    @PostMapping
+    public Integer saveFile(MultipartFile fileUpload) {
+        return null;
     }
 }
 
