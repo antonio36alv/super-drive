b3ea5ab10f142721d9329f1b422e139cd642bf5f
antonio36alv <ant.36alv@gmail.com>
2020-11-11 13:26:30 -0500

    implemented copy btn, show/hide alternates image

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,7 +1,15 @@
 const showPasswordBtns = document.getElementsByClassName("show-password-btn");
+const copyPasswordBtns = document.getElementsByClassName("copy-password-btn");
+
+const originalClassesShowBtn = showPasswordBtns[0].getAttribute("class");
+const originalClassesCopyBtn = copyPasswordBtns[0].getAttribute("class");
 
 Array.prototype.map.call(showPasswordBtns, element => 
     element.addEventListener("click", showPassword)
+);
+
+Array.prototype.map.call(copyPasswordBtns, element => 
+    element.addEventListener("click", copyPassword)
 );
 
 // For opening the note modal
@@ -34,8 +42,49 @@ function showCredentialModal(credentialId, url, username, password) {
     $('#credentialModal').modal('show');
 }
 
+/*
+    gets id from event and finds the password input
+    alternates password input's type between text and password
+*/
 function showPassword(e) {
     const id = e.target.id.split("-")[3];
-    const passwordInput = document.getElementById(`user-password-${id}`);
-    passwordInput.getAttribute("type") === "password" ? passwordInput.setAttribute("type", "text") : passwordInput.setAttribute("type", "password")
+    const passwordInput = getPasswordInput(id); 
+    const clickedShowPasswordBtn = showPasswordBtns[id - 1];
+    if(passwordInput.getAttribute("type") === "password") {
+        passwordInput.setAttribute("type", "text");
+        // document.getElementById(`show-password-btn-${e.target.id}`).setAttribute("btn fa fa-eye-slash show-password-btn")
+        clickedShowPasswordBtn.setAttribute("class", "btn fa fa-eye-slash show-password-btn");
+    } else {
+        passwordInput.setAttribute("type", "password");
+        clickedShowPasswordBtn.setAttribute("class", originalClassesShowBtn);
+    }
+}
+
+/*        
+    gets value from password input and copies it to user's clipboard
+*/
+function copyPassword(e) {
+    const passwordInput = getPasswordInput(e.target.id.split("-")[3]); 
+    const value = passwordInput.value;
+    const placeholder = document.createElement("input");
+    document.body.append(placeholder);
+    placeholder.value = value;
+    placeholder.select();
+    placeholder.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    document.body.removeChild(placeholder);
+    // TODO confirm that it has been copied
+    e.target.textContent = "Copied!";
+    e.target.setAttribute("class", "btn show-password-btn");
+    setTimeout(() => {
+        e.target.textContent = "";
+        e.target.setAttribute("class", originalClassesCopyBtn);
+    }, 800)
+}
+
+/*
+    finds and returns corresponding passwordInput
+*/
+function getPasswordInput(id) {
+    return document.getElementById(`user-password-${id}`);
 }
