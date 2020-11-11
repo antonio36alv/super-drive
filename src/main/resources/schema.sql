105b30482d5be0621c52e946dc503ba1b92c9325
antonio36alv <ant.36alv@gmail.com>
2020-11-10 20:26:40 -0500

    configured for properly for diff envs

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS USERS (
   userid INT PRIMARY KEY auto_increment,
   username VARCHAR(20),
-  salt VARCHAR,
-  password VARCHAR,
+  salt VARCHAR(100),
+  password VARCHAR(100),
   firstname VARCHAR(20),
   lastname VARCHAR(20)
 );
@@ -17,9 +17,9 @@ CREATE TABLE IF NOT EXISTS NOTES (
 
 CREATE TABLE IF NOT EXISTS FILES (
     fileId INT PRIMARY KEY auto_increment,
-    filename VARCHAR,
-    contenttype VARCHAR,
-    filesize VARCHAR,
+    filename VARCHAR(100),
+    contenttype VARCHAR(100),
+    filesize VARCHAR(100),
     userid INT,
     filedata BLOB,
     foreign key (userid) references USERS(userid)
@@ -29,8 +29,8 @@ CREATE TABLE IF NOT EXISTS CREDENTIALS (
     credentialid INT PRIMARY KEY auto_increment,
     url VARCHAR(100),
     username VARCHAR (30),
-    key VARCHAR,
-    password VARCHAR,
+    ekey VARCHAR(100),
+    password VARCHAR(100),
     userid INT,
     foreign key (userid) references USERS(userid)
 );
